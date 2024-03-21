a4a8ea4816fea5d93f00ce2820ff256418ea7105
ravengaspar <ravengaspar@gmail.com>
2024-03-21 09:32:25 -0400

    Finish setting up next auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
 import { ModeToggle } from "@/components/mode-toggle";
+import { Providers } from "./providers";
+import { Header } from "./header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,19 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-      <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <div>
-              <ModeToggle />
-            </div>
-            {children}
-          </ThemeProvider>
-        </body>
+    <body className={inter.className}>
+    <Providers>
+    <Header />
+    {children}
+    </Providers>
+    </body>
     </html>
-  );
-}
+    );
+  }
+  
